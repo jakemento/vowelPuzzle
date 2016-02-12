4a84c39c6a9d7a75866806f5aa334919e5fb7ad5
Jake Steinberg <jakesteinberg@gmail.com>
2016-02-12 08:21:07 -0800

    add business logic and correct variable names

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 import java.io.*;
 
-import org.apache.commons.lang.StringUtils;
+// import org.apache.commons.lang.StringUtils;
 
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
@@ -9,24 +9,24 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-    // String layout = "templates/layout.vtl";
-    //
-    // get("/", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/home.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/results", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/detector.vtl");
-    //
-    //   String userSentence = request.queryParams("sentence");
-    //
-    //   String alteredSentence = replaceWord(userSentence, wordToReplace, replaceWith);
-    //   model.put("alteredSentence", alteredSentence);
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/results", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/detector.vtl");
+
+      String userSentence = request.queryParams("sentence");
+
+      String alteredSentence = replaceVowels(userSentence);
+      model.put("alteredSentence", alteredSentence);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 
 
